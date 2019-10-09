daeb2e3b1301bc793bd94e9f7bc3df55db19b565
thswlrud0 <thswlrud0@naver.com>
2019-10-10 00:49:34 +0900

    search, map , 로그인 화면과 버튼만 구현

--- android_lot/app/src/main/java/com/example/myapplication/view/SearchActivity.java
+++ android_lot/app/src/main/java/com/example/myapplication/view/SearchActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.myapplication.R;
 
@@ -12,11 +16,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity{
 
+    EditText searchline;
+    ImageButton searchbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
 
+        searchbutton = (ImageButton) findViewById(R.id.search_button);
+        searchline = (EditText) findViewById(R.id.search_line);
+
+        searchbutton.setOnClickListener(new Button.OnClickListener(){
+
+            String searchstr = searchline.getText().toString();
+
+            @Override
+            public void onClick(View v) {
+                /* search 값을 */
+
+
+            }
+        });
 
     }
 
